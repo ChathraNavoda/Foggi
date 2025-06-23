6507e8a645abd7efe40c4b64239041f53e093567
ChathraNavoda <chathra96@gmail.com>
2025-06-23 20:50:39 +0530

    flutterfire config and firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:foggi/core/theme/theme_provider.dart';
+import 'package:foggi/firebase_options.dart';
 
 import 'app/router.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: FoggiApp()));
 }
 
